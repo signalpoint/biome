71b7d55e4613cdb531ce7edcdd6adb39ce28129a
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2023-03-06 00:11:33 +0000

    Support 2nd elevation to render 4 panes

--- js/bm.js
+++ js/bm.js
@@ -27,24 +27,83 @@ var blockHeight = 64
 
 // Can we make bigger chunks? i.e. zoom out to level 2, see 4 chunks, instead of 1
 // 1024x768
-var map = [
-  'w','w','w','w','w','w','w','w','w','w','w','w','w','w','w','w',
-  'w','l','l','l','l','l','l','l','l','l','l','l','l','l','l','w',
-  'w','l','l','l','l','l','l','l','l','l','l','l','l','l','l','w',
-  'w','l','l','l','l','l','l','l','l','l','l','l','l','l','l','w',
-  'w','l','l','l','l','l','l','l','l','l','l','l','l','l','l','w',
-  'w','l','l','l','l','l','l','l','l','l','l','l','l','l','l','w',
-  'w','l','l','l','l','l','l','l','l','l','l','l','l','l','l','w',
-  'w','l','l','l','l','l','l','l','l','l','l','l','l','l','l','w',
-  'w','l','l','l','l','l','l','l','l','l','l','l','l','l','l','w',
-  'w','l','l','l','l','l','l','l','l','l','l','l','l','l','l','w',
-  'w','l','l','l','l','l','l','l','l','l','l','l','l','l','l','w',
-  'w','w','w','w','w','w','w','w','w','w','w','w','w','w','w','w'
-]
 var colorMap = {
-  w: '#90e0ef',
-  l: '#ffe6a7'
+  o: '#22577a', // ocean
+  w: '#90e0ef', // water
+  g: '#25a244', // grass
+  s: '#ffe6a7' // sand
 }
+
+let elevation = 1
+let maxElevation = 2
+
+var map = [
+
+  // Big Island
+  [
+    'w','w','w','w','w','w','w','w','w','w','w','w','w','w','w','w',
+    'w','s','s','s','s','s','s','s','s','s','s','s','s','s','s','w',
+    'w','s','s','s','s','s','s','s','s','s','s','s','s','s','s','w',
+    'w','s','s','s','s','s','s','s','s','s','s','s','s','s','s','w',
+    'w','s','s','s','s','s','s','s','s','s','s','s','s','s','s','w',
+    'w','s','s','s','s','s','s','s','s','s','s','s','s','s','s','w',
+    'w','s','s','s','s','s','s','s','s','s','s','s','s','s','s','w',
+    'w','s','s','s','s','s','s','s','s','s','s','s','s','s','s','w',
+    'w','s','s','s','s','s','s','s','s','s','s','s','s','s','s','w',
+    'w','s','s','s','s','s','s','s','s','s','s','s','s','s','s','w',
+    'w','s','s','s','s','s','s','s','s','s','s','s','s','s','s','w',
+    'w','w','w','w','w','w','w','w','w','w','w','w','w','w','w','w'
+  ],
+
+  // Medium Island
+  [
+    'w','w','w','w','w','w','w','w','w','w','w','w','w','w','w','w',
+    'w','w','w','w','w','w','w','w','w','w','w','w','w','w','w','w',
+    'w','w','s','s','s','s','s','s','s','s','s','s','s','s','w','w',
+    'w','w','s','s','s','s','s','s','s','s','s','s','s','s','w','w',
+    'w','w','s','s','s','s','s','s','s','s','s','s','s','s','w','w',
+    'w','w','s','s','s','s','s','s','s','s','s','s','s','s','w','w',
+    'w','w','s','s','s','s','s','s','s','s','s','s','s','s','w','w',
+    'w','w','s','s','s','s','s','s','s','s','s','s','s','s','w','w',
+    'w','w','s','s','s','s','s','s','s','s','s','s','s','s','w','w',
+    'w','w','s','s','s','s','s','s','s','s','s','s','s','s','w','w',
+    'w','w','w','w','w','w','w','w','w','w','w','w','w','w','w','w',
+    'w','w','w','w','w','w','w','w','w','w','w','w','w','w','w','w'
+  ],
+
+  // Small Island
+  [
+    'w','w','w','w','w','w','w','w','w','w','w','w','w','w','w','w',
+    'w','w','w','w','w','w','w','w','w','w','w','w','w','w','w','w',
+    'w','w','w','w','w','w','w','w','w','w','w','w','w','w','w','w',
+    'w','w','w','s','s','s','s','s','s','s','s','s','s','w','w','w',
+    'w','w','w','s','g','g','g','g','g','g','g','g','s','w','w','w',
+    'w','w','w','s','g','g','g','g','g','g','g','g','s','w','w','w',
+    'w','w','w','s','g','g','g','g','g','g','g','g','s','w','w','w',
+    'w','w','w','s','g','g','g','g','g','g','g','g','s','w','w','w',
+    'w','w','w','s','s','s','s','s','s','s','s','s','s','w','w','w',
+    'w','w','w','w','w','w','w','w','w','w','w','w','w','w','w','w',
+    'w','w','w','w','w','w','w','w','w','w','w','w','w','w','w','w',
+    'w','w','w','w','w','w','w','w','w','w','w','w','w','w','w','w'
+  ],
+
+  // Ocean
+  [
+    'w','w','w','w','w','w','w','w','w','w','w','w','w','w','w','w',
+    'w','w','w','w','w','w','w','w','w','w','w','w','w','w','w','w',
+    'w','w','w','w','w','w','w','w','w','w','w','w','w','w','w','w',
+    'w','w','w','w','w','w','w','w','w','w','w','w','w','w','w','w',
+    'w','w','w','w','w','w','w','w','w','w','w','w','w','w','w','w',
+    'w','w','w','w','w','w','w','w','w','w','w','w','w','w','w','w',
+    'w','w','w','w','w','w','w','w','w','w','w','w','w','w','w','w',
+    'w','w','w','w','w','w','w','w','w','w','w','w','w','w','w','w',
+    'w','w','w','w','w','w','w','w','w','w','w','w','w','w','w','w',
+    'w','w','w','w','w','w','w','w','w','w','w','w','w','w','w','w',
+    'w','w','w','w','w','w','w','w','w','w','w','w','w','w','w','w',
+    'w','w','w','w','w','w','w','w','w','w','w','w','w','w','w','w'
+  ]
+
+];
 
 window.addEventListener('load', function() {
 
@@ -52,8 +111,8 @@ window.addEventListener('load', function() {
   canvas = document.getElementById("biome-bloom")
 
   // 720p
-  canvas.width = 1280
-  canvas.height = 720
+//  canvas.width = 1280
+//  canvas.height = 720
 
   // 1024*768
   canvas.width = 1024
@@ -64,7 +123,7 @@ window.addEventListener('load', function() {
 
   // Canvas click listener.
   canvas.addEventListener('mousedown', function(e) {
-    console.log('mousedown');
+//    console.log('mousedown');
     mouseDown = true;
     const rect = canvas.getBoundingClientRect()
     const x = e.clientX - rect.left
@@ -75,10 +134,24 @@ window.addEventListener('load', function() {
   canvas.addEventListener('wheel', function(e) {
 //    console.log(e);
     if (e.deltaY > 0) {
-      console.log('zoom out');
+
+//      console.log('zoom out');
+
+      if (elevation < maxElevation) {
+        elevation++
+        drawMap();
+      }
+
     }
     else {
-      console.log('zoom in');
+
+//      console.log('zoom in');
+
+      if (elevation !== 1) {
+        elevation--
+        drawMap();
+      }
+
     }
     return false;
   }, false);
@@ -108,18 +181,119 @@ window.addEventListener('load', function() {
 
 });
 
+function clearMap() {
+  c.clearRect(0, 0, canvas.width, canvas.height);
+}
+
 function drawMap() {
+
+  clearMap();
+
+  console.log('elevation', elevation);
+
   let x = 0
   let y = 0
-  for (var i = 0; i < map.length; i++) {
-    var block = map[i]
-    c.fillStyle = colorMap[block];
-    c.fillRect(x, y, blockWidth, blockHeight);
-    x += blockWidth
-    if (x >= canvas.width) {
-      x = 0
-      y += blockHeight
+
+  if (elevation === 1) {
+
+    var chunk = map[0];
+    for (var i = 0; i < chunk.length; i++) {
+      var block = chunk[i]
+      c.fillStyle = colorMap[block];
+      c.fillRect(x, y, blockWidth, blockHeight);
+      x += blockWidth
+      if (x >= canvas.width) {
+        x = 0
+        y += blockHeight
+      }
     }
+
+  }
+  else {
+
+    let z = Math.pow(2, elevation)
+    console.log('z', z);
+
+    let modX = 0
+    let modY = 0
+
+    for (var pane = 0; pane < z; pane++) {
+
+      x = modX
+      y = modY
+
+      let mod = pane % elevation
+
+      if (mod) { // odd... (when elevation is 2)
+
+        modX = canvas.width / elevation
+        x = modX
+
+      }
+      else { // even... (when elevation is 2)
+
+        if (pane) {
+
+          modY = canvas.height / elevation
+          y = modY
+
+        }
+
+      }
+
+      console.log('------------------------');
+      console.log('pane', pane);
+      console.log('x, y');
+      console.log(x, y);
+      console.log('mod', mod);
+      console.log('modX', modX);
+      console.log('modY', modY);
+
+      var chunk = map[pane];
+
+      for (var i = 0; i < chunk.length; i++) {
+
+        var block = chunk[i];
+
+        // Draw the block on the canvas.
+        c.fillStyle = colorMap[block];
+        c.fillRect(x, y, blockWidth / elevation, blockHeight / elevation);
+
+        // Move over to get ready to draw the next block.
+        x += (blockWidth / elevation)
+
+        if (mod) { // odd... (when elevation is 2)
+
+          if (x >= canvas.width) {
+            x = modX
+            y += (blockHeight / elevation)
+          }
+
+        }
+        else { // even... (when elevation is 2)
+
+          if (x >= (canvas.width / elevation)) {
+            x = 0
+            y += (blockHeight / elevation)
+          }
+
+        }
+
+      }
+
+      console.log('finished pane', pane);
+
+      if (mod === 1) {
+
+        console.log('last pane in row');
+
+        modX = 0
+        x = modX
+
+      }
+
+    }
+
   }
 }
 

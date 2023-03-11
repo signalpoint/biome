e9604c7659aac919d1c5b0cc6ea0f0e4d28d1f4c
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2023-03-11 00:45:13 +0000

    Add 'type' as a Block property

--- js/Block.js
+++ js/Block.js
@@ -1,13 +1,17 @@
 class Block {
 
-  constructor(delta) {
+  constructor({
+    delta,
+    type
+  }) {
 
     this.delta = delta
+    this.type = type
 
   }
 
   // abstracts
-  
+
   update() {}
 
   draw(x, y) {}

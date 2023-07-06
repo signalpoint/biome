4a208c6b62149a090a7af92438eeb59d71ec6de7
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2023-07-06 14:13:59 +0000

    Add x(), y(), moveUp(), moveDown(), moveLeft(), moveRight() and move() to DesignerCamera

--- js/DesignerCamera.js
+++ js/DesignerCamera.js
@@ -2,14 +2,27 @@ class DesignerCamera {
 
   constructor() {
 
+    this._x = 0
+    this._y = 0
+
   }
 
-  getStartX() {
-    return 0
-  }
+  x() { return this._x }
+  y() { return this._y }
 
-  getStartY() {
-    return 0
+  moveUp() { this._y-- }
+  moveDown() { this._y++ }
+  moveLeft() { this._x-- }
+  moveRight() { this._x++ }
+
+  move(direction) {
+    switch (direction) {
+      case 'up': if (this.y() > 0) { this.moveUp(); } break;
+      case 'down': this.moveDown(); break;
+      case 'left': if (this.x() > 0) { this.moveLeft(); } break;
+      case 'right': this.moveRight(); break;
+    }
+    refresh()
   }
 
 }

f34af4dc6c64cca1201d38dd8ac6b3bbf1f899df
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2023-07-06 22:51:34 +0000

    Add selected, select() and deselect() to Block

--- js/Block.js
+++ js/Block.js
@@ -2,11 +2,20 @@ class Block {
 
   constructor({
     delta,
-    type
+    type,
+    selected = false
+//    edges = null
   }) {
 
     this.delta = delta
     this.type = type
+    this.selected = selected
+//    this.edges = edges ? edges : {
+//      t: 0, // top
+//      b: 0, // bottom
+//      s: 0, // start
+//      e: 0 // end
+//    }
 
   }
 
@@ -15,5 +24,8 @@ class Block {
   update() {}
 
   draw(x, y) {}
+
+  select() { this.selected = true }
+  deselect() { this.selected = false }
 
 }

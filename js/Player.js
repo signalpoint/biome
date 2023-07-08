98696d343d4e0eaa3c2301738ed2c7e9fa69f98d
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2023-07-08 20:13:30 +0000

    In Player.update(), determine which directions the player can move before allowing them to move. Add Player collision states and related methods.

--- js/Player.js
+++ js/Player.js
@@ -29,6 +29,12 @@ class Player {
         down: false,
         left: false,
         right: false
+      },
+      collision: {
+        top: false,
+        bottom: false,
+        left: false,
+        right: false
       }
     }
 
@@ -45,10 +51,32 @@ class Player {
 
 //    console.log('player velocity: ' + this.vX + ', ' + this.vY)
 
+    // determine if the player can move up, down, left and/or right...
+
+    let upwardDestination = this.y - 1
+    let downwardDestination = this.y + this.height  + 1
+    let leftDestination = this.x - 1
+    let rightDestination = this.x + this.width + 1
+
+    let blockAboveTopLeft = d.blocks[d.getBlockDelta(this.x, upwardDestination)]
+    let blockAboveTopRight = d.blocks[d.getBlockDelta(this.x + this.width, upwardDestination)]
+    let blockBelowBottomLeft = d.blocks[d.getBlockDelta(this.x, downwardDestination)]
+    let blockBelowBottomRight = d.blocks[d.getBlockDelta(this.x + this.width, downwardDestination)]
+
+    let blockBeforeTopLeft = d.blocks[d.getBlockDelta(leftDestination, this.y)]
+    let blockAfterTopRight = d.blocks[d.getBlockDelta(rightDestination, this.y)]
+    let blockBeforeBottomLeft = d.blocks[d.getBlockDelta(leftDestination, this.y + this.height)]
+    let blockAfterBottomRight = d.blocks[d.getBlockDelta(rightDestination, this.y + this.height)]
+
+    let canMoveUp = blockAboveTopLeft && !blockAboveTopLeft.solid && blockAboveTopRight && !blockAboveTopRight.solid
+    let canMoveDown = blockBelowBottomLeft && !blockBelowBottomLeft.solid && blockBelowBottomRight && !blockBelowBottomRight.solid
+    let canMoveLeft = blockBeforeTopLeft && !blockBeforeTopLeft.solid && blockBeforeBottomLeft && !blockBeforeBottomLeft.solid
+    let canMoveRight = blockAfterTopRight && !blockAfterTopRight.solid && blockAfterBottomRight && !blockAfterBottomRight.solid
+
     // key press velocity changes for player and player movement states
 
     // up
-    if (keys.up.pressed && Math.abs(this.vY) < this.maxVelocityY) {
+    if (canMoveUp && keys.up.pressed && Math.abs(this.vY) < this.maxVelocityY) {
       this.state.moving.up = true
       this.vY -= 1
     }
@@ -60,7 +88,7 @@ class Player {
     }
 
     // down
-    if (keys.down.pressed && Math.abs(this.vY) < this.maxVelocityY) {
+    if (canMoveDown && keys.down.pressed && Math.abs(this.vY) < this.maxVelocityY) {
       this.state.moving.down = true
       this.vY += 1
     }
@@ -72,7 +100,7 @@ class Player {
     }
 
     // left
-    if (keys.left.pressed && Math.abs(this.vX) < this.maxVelocityX) {
+    if (canMoveLeft && keys.left.pressed && Math.abs(this.vX) < this.maxVelocityX) {
       this.state.moving.left = true
       this.vX -= 1
     }
@@ -84,7 +112,7 @@ class Player {
     }
 
     // right
-    if (keys.right.pressed && Math.abs(this.vX) < this.maxVelocityX) {
+    if (canMoveRight && keys.right.pressed && Math.abs(this.vX) < this.maxVelocityX) {
       this.state.moving.right = true
       this.vX += 1
     }
@@ -98,6 +126,8 @@ class Player {
     // if player has velocity in either direction, change their position accordingly
     if (this.vX !== 0) { this.x += this.vX }
     if (this.vY !== 0) { this.y += this.vY }
+//    if (this.vX !== 0 && (canMoveLeft || canMoveRight)) { this.x += this.vX }
+//    if (this.vY !== 0 && (canMoveUp || canMoveDown)) { this.y += this.vY }
 
     dPlayer.refreshCoordinates()
 
@@ -111,6 +141,46 @@ class Player {
       this.width,
       this.height
     )
+  }
+
+  isMovingUp() { return this.state.moving.up }
+  isMovingDown() { return this.state.moving.down }
+  isMovingLeft() { return this.state.moving.left }
+  isMovingRight() { return this.state.moving.right }
+
+  hasTopCollision() { return this.state.collision.top }
+  hasBottomCollision() { return this.state.collision.bottom }
+  hasLeftCollision() { return this.state.collision.left }
+  hasRightCollision() { return this.state.collision.right }
+
+  getBlockDeltaFromTopLeftPosition() { return d.getBlockDelta(this.x, this.y) }
+  getBlockDeltaFromTopRightPosition() { return d.getBlockDelta(this.x + this.width, this.y) }
+  getBlockDeltaFromBottomLeftPosition() { return d.getBlockDelta(this.x, this.y + this.height) }
+  getBlockDeltaFromBottomRightPosition() { return d.getBlockDelta(this.x + this.width, this.y + this.height) }
+
+  getBlockDeltasFromPosition() {
+
+    let topLeft = this.getBlockDeltaFromTopLeftPosition()
+    let topRight = this.getBlockDeltaFromTopRightPosition()
+    let bottomLeft = this.getBlockDeltaFromBottomLeftPosition()
+    let bottomRight = this.getBlockDeltaFromBottomRightPosition()
+
+    let deltas = [topLeft]
+    if (!deltas.includes(topRight)) { deltas.push(topRight) }
+    if (!deltas.includes(bottomLeft)) { deltas.push(bottomLeft) }
+    if (!deltas.includes(bottomRight)) { deltas.push(bottomRight) }
+
+    return deltas
+
+  }
+
+  resetCollisionStates() {
+    this.state.collision = {
+      top: false,
+      bottom: false,
+      left: false,
+      right: false
+    }
   }
 
 }

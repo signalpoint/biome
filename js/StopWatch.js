971508e4bce3a13df87b15f4e230c265659a30fb
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2023-07-11 19:39:14 +0000

    Add StopWatch.js

---
+++ js/StopWatch.js
@@ -0,0 +1,41 @@
+class StopWatch {
+
+  constructor() {
+
+    this.startTime = 0
+    this.running = false
+    this.elapsed = 0
+
+  }
+
+  start() {
+    this.setStartTime(+new Date())
+    this.running = true
+  }
+
+  stop() {
+    this.elapsed = (+new Date()) - this.getStartTime()
+    this.running = false
+  }
+
+  setStartTime(startTime) {
+    this.startTime = startTime
+  }
+
+  getStartTime() {
+    return this.startTime
+  }
+
+  isRunning() {
+    return this.running
+  }
+
+  reset() {
+    this.elapsed = 0
+  }
+
+  getElapsedTime() {
+    return this.isRunning() ? (+new Date()) - this.getStartTime() : this.elapsed
+  }
+
+};

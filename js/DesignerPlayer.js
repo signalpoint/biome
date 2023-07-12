1404569c5575cf3f40654c0bea2ad86e683f769b
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2023-07-12 02:04:11 +0000

    in Player, add badges and refresh functions for the player's blocks, coordinates and block delta

--- js/DesignerPlayer.js
+++ js/DesignerPlayer.js
@@ -4,6 +4,9 @@ class DesignerPlayer {
 
     this.playerCoordinatesBadge = document.querySelector('#playerCoordinatesBadge')
     this.playerVelocityBadge = document.querySelector('#playerVelocityBadge')
+    this.playerBlocksBadge = document.querySelector('#playerBlocksBadge')
+    this.playerBlockCoordinatesBadge = document.querySelector('#playerBlockCoordinatesBadge')
+    this.playerBlockDeltaFromPositionBadge = document.querySelector('#playerBlockDeltaFromPositionBadge')
 
   }
 
@@ -13,6 +16,20 @@ class DesignerPlayer {
 
   refreshVelocityBadge() {
     this.playerVelocityBadge.innerHTML = player.vX + ', ' + player.vY
+  }
+
+  refreshBlocksBadge() {
+    this.playerBlocksBadge.innerHTML = player.getBlockDeltasFromPosition().join(', ')
+  }
+
+  refreshBlockCoordinatesBadge() {
+    let pos = d.getBlockCoords(player.x, player.y)
+    this.playerBlockCoordinatesBadge.innerHTML = `${pos.x}, ${pos.y}`
+  }
+
+  refreshBlockDeltaFromPositionBadge() {
+    let pos = d.getBlockCoords(player.x, player.y)
+    this.playerBlockDeltaFromPositionBadge.innerHTML = d.getBlockDeltaFromPos(pos.x, pos.y)
   }
 
 }

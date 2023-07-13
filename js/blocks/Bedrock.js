f9d69e17edf3b3fc794090602aa2f92b431cb031
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2023-07-13 01:48:35 +0000

    Rename doc title. Add Bedrock.js. Give the belt container an id and add an attribute to each belt button.

---
+++ js/blocks/Bedrock.js
@@ -0,0 +1,32 @@
+class Bedrock extends Block {
+
+  constructor({
+    delta,
+    type,
+    selected = 0,
+    solid = 0
+  }) {
+
+    super({
+      delta,
+      type,
+      selected,
+      solid
+    })
+
+  }
+
+  update() {
+
+  }
+
+  draw(x, y) {
+
+    c.beginPath()
+    c.rect(x * d.getBlockSize(), y * d.getBlockSize(), d.getBlockSize(), d.getBlockSize())
+    c.fillStyle = this.solid ? '#111' : '#222'
+    c.fill()
+
+  }
+
+}

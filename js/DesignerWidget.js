3be7f25e2aa9eeb11dd079a6ab325259058b3e45
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2023-07-15 03:56:20 +0000

    In DesignerWidget, add init(), getLegend(), getTitle(), setTitle(), getCloseBtn(), setPane() and setPaneContent(). Abstract out designerWidgetCloseBtnOnclick() and use it. Support loading of building DesignerWidget from storage.

--- js/DesignerWidget.js
+++ js/DesignerWidget.js
@@ -13,11 +13,20 @@ class DesignerWidget {
     this._offsetY = null
   }
 
+  init() {
+    this.getLegend().addEventListener('mousedown', designerWidgetDragMouseDown) // handle widget drag
+    this.getCloseBtn().addEventListener('click', designerWidgetCloseBtnOnclick)
+  }
+
   getElement() { return document.getElementById(this.id) }
   getNav() { return this.getElement().querySelector('.designer-widget-nav') }
   getNavBtns() { return this.getNav().querySelectorAll('[data-op]') }
   getNavBtn(op) { return this.getNav().querySelector(`[data-op="${op}"]`) }
   getActiveNavBtn() { return this.getNav().querySelector('.active') }
+  getLegend() { return this.getElement().querySelector('legend') }
+  getTitle() { return this.getLegend().querySelector('span').innerHTML }
+  setTitle(title) { this.getLegend().querySelector('span').innerHTML = title }
+  getCloseBtn() { return this.getElement().querySelector('.designer-widget-close') }
 
   save() { _designerWidgets[this.id] = this }
 
@@ -44,6 +53,7 @@ class DesignerWidget {
   setOffsetY(y) { this._offsetY = y }
 
   getPanes() { return this.getElement().querySelector('.designer-widget-panes') }
+  getPane(op) { return this.getPanes().querySelector(`[data-op="${op}"]`) }
   showPane(op) {
 
     // change nav
@@ -56,7 +66,7 @@ class DesignerWidget {
       panes.children[i].classList.add('d-none')
       panes.children[i].classList.remove('active')
     }
-    let activePane = panes.querySelector(`[data-op="${op}"]`)
+    let activePane = this.getPane(op)
     activePane.classList.remove('d-none')
 
     // set op
@@ -65,6 +75,10 @@ class DesignerWidget {
     // update mode
     d.setMode(op)
 
+  }
+
+  setPaneContent(op, html) {
+    this.getPane(op).innerHTML = html
   }
 
   move(x, y) {
@@ -154,7 +168,7 @@ function initDesignerWidgets() {
     widget.save()
 
     // handle widget drag
-    legend.addEventListener('mousedown', designerWidgetDragMouseDown);
+    legend.addEventListener('mousedown', designerWidgetDragMouseDown)
 
   }
 
@@ -180,24 +194,7 @@ function initDesignerWidgets() {
   // widget close buttons
   let designerWidgetCloseBtns = document.querySelectorAll('.designer-widget-close')
   for (var i = 0; i < designerWidgetCloseBtns.length; i++) {
-    designerWidgetCloseBtns[i].addEventListener('click', function(e) {
-      let closeBtn = e.target
-      let fieldset = null
-      let parent = closeBtn.parentNode
-      while (parent) {
-        if (parent.classList.contains('designer-widget')) {
-          fieldset = parent
-          break
-        }
-        parent = parent.parentNode
-      }
-      if (fieldset) {
-        let widgetId = fieldset.getAttribute('id')
-        let widget = loadDesignerWidget(widgetId)
-        widget.hide()
-        saveDesignerWidgets()
-      }
-    })
+    designerWidgetCloseBtns[i].addEventListener('click', designerWidgetCloseBtnOnclick)
   }
 
   // DesignerWidgetNav buttons
@@ -249,15 +246,53 @@ function initDesignerWidgets() {
 
 }
 
+function designerWidgetCloseBtnOnclick(e) {
+  let closeBtn = e.target
+  let fieldset = null
+  let parent = closeBtn.parentNode
+  while (parent) {
+    if (parent.classList.contains('designer-widget')) {
+      fieldset = parent
+      break
+    }
+    parent = parent.parentNode
+  }
+  if (fieldset) {
+    let widgetId = fieldset.getAttribute('id')
+    let widget = loadDesignerWidget(widgetId)
+    widget.hide()
+    saveDesignerWidgets()
+  }
+}
+
 function loadDesignerWidgets() {
+
   let input = dStorage.load('designerWidgets')
+
   for (let id in input) {
+
     if (!input.hasOwnProperty(id)) { continue }
+
     let data = input[id]
-    let widget = new DesignerWidget({
-      id
-    });
-    widget.save()
+    let widget = null
+
+    if (document.getElementById(id)) { // widget exists in DOM already
+      widget = new DesignerWidget({
+        id
+      });
+      widget.save()
+    }
+    else { // widget doesn't exist in DOM...
+
+      // building widgets
+      // clone's DOM element template
+      if (id.indexOf('buildingWidget') === 0) {
+        let delta = id.replace('buildingWidget', '')
+        widget = createBuildingWidget(delta)
+      }
+
+    }
+
     if (data.open) { widget.show() }
     if (data.op) { widget.showPane(data.op) }
     if (data.oX && data.oY) {
@@ -265,7 +300,9 @@ function loadDesignerWidgets() {
       widget.setOffsetX(data.oX)
       widget.setOffsetY(data.oY)
     }
+
   }
+
 }
 
 function saveDesignerWidgets() {

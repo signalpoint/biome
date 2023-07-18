8b3ff63dd9cca79af87ef1d4bb99acdcc45c64fe
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2023-07-18 03:30:38 +0000

    Add Border.js

---
+++ js/blocks/Border.js
@@ -0,0 +1,32 @@
+class Border extends Block {
+
+  constructor({
+    delta,
+    type,
+    selected = 0,
+    solid = 1
+  }) {
+
+    super({
+      delta,
+      type,
+      selected,
+      solid
+    })
+
+  }
+
+  update() {
+
+  }
+
+  draw(x, y) {
+
+    c.beginPath()
+    c.rect(x * d.getBlockSize(), y * d.getBlockSize(), d.getBlockSize(), d.getBlockSize())
+    c.fillStyle = '#000'
+    c.fill()
+
+  }
+
+}

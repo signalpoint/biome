d96b7f97c7df9d408d522efc9125c9844be3e6e7
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2023-07-19 17:05:42 +0000

    WIP add pickaxe card to builders workshop

--- js/buildings/BuildersWorkshop.js
+++ js/buildings/BuildersWorkshop.js
@@ -32,4 +32,34 @@ class BuildersWorkshop extends Building {
 
   }
 
+  getPaneContent() {
+    let html =
+
+      // Pickaxe
+      `<div class="card" style="width: 18rem;">
+        <div class="card-body">
+          <h5 class="card-title">Pickaxe</h5>
+          <p class="card-text">...</p>
+          <a href="#" class="btn btn-primary" title="Build a Pickaxe">
+            Build
+            <i class="fas fa-share float-end ms-2"></i>
+          </a>
+        </div>
+      </div>`
+
+    // LumberCamp
+//    `<div class="card" style="width: 18rem;">
+//        <div class="card-body">
+//          <h5 class="card-title">Lumber Camp</h5>
+//          <p class="card-text">...</p>
+//          <a href="#" class="btn btn-primary" title="Build a Lumber Camp">
+//            Build
+//            <i class="fas fa-share float-end ms-2"></i>
+//          </a>
+//        </div>
+//      </div>`
+
+    return html
+  }
+
 }

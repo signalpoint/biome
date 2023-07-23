e529d80b6876b4d2bfb08ac23d5ee3a75393c864
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2023-07-23 14:40:04 +0000

    In Block, add health and canvasMouseDownInterval properties, have update(), mine the block if it runs out of health, add hit() and an empty canvasMouseDownHoldListener

--- js/Block.js
+++ js/Block.js
@@ -4,13 +4,17 @@ class Block {
     delta,
     type,
     selected = 0,
-    solid = 0
+    solid = 0,
+    health = 100,
+    canvasMouseDownInterval = 500
   }) {
 
     this.delta = delta
     this.type = type
     this.selected = selected
     this.solid = solid
+    this.health = health
+    this.canvasMouseDownInterval = canvasMouseDownInterval
 
     //d.addBlockToIndex(this)
 
@@ -18,9 +22,22 @@ class Block {
 
   // abstracts
 
-  update() {}
+  update() {
+
+    // mine the block (if it ran out of health)
+    if (!this.health) {
+      player.mineBlock(this.delta)
+    }
+
+  }
 
   draw(x, y) {}
+
+  hit(force) {
+//    console.log(`block: ${this.health} - ${force}`)
+    this.health -= force
+    if (this.health < 0) { this.health = 0 }
+  }
 
   select() { this.selected = 1 }
   deselect() { this.selected = 0 }
@@ -38,6 +55,11 @@ class Block {
       if (player.isMovingRight()) { player.state.collision.right = true }
     }
 
+  }
+
+  canvasMouseDownHoldListener(e) {
+//    if (mouse.left.pressed) { console.log('left hold', mouse.left.timer.getElapsedTime()) }
+//    if (mouse.right.pressed) { console.log('right hold', mouse.right.timer.getElapsedTime()) }
   }
 
 }

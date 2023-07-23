b4b48283b36f614e5aab79b475876a247de07ef5
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2023-07-23 15:02:52 +0000

    WIP Add canvas alpha effect when mining a block

--- js/blocks/Grass.js
+++ js/blocks/Grass.js
@@ -11,12 +11,9 @@ class Grass extends Block {
       delta,
       type,
       selected,
-      solid
+      solid,
+      canvasMouseDownInterval: 210 // essentially the axe attack speed
     })
-
-  }
-
-  update() {
 
   }
 
@@ -26,6 +23,25 @@ class Grass extends Block {
     c.rect(x * d.getBlockSize(), y * d.getBlockSize(), d.getBlockSize(), d.getBlockSize())
     c.fillStyle = this.solid ? '#1a7431' : '#25a244'
     c.fill()
+
+    if (this.health < 100) {
+
+      c.globalAlpha = (100 - this.health) * .01
+      c.fillStyle = 'black'
+      c.fillRect(x * d.getBlockSize(), y * d.getBlockSize(), d.getBlockSize(), d.getBlockSize())
+      c.globalAlpha = 1.0;
+
+//      let maxRadius = d.getBlockSize() / 2
+//      let radius = maxRadius / this.health
+//      c.beginPath()
+//      c.arc(
+//        x * d.getBlockSize() + d.getBlockSize() / 2,
+//        y * d.getBlockSize() + d.getBlockSize() / 2,
+//        radius, 0, 2 * Math.PI
+//      )
+//      c.fill()
+
+    }
 
   }
 

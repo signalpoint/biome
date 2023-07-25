ac4e2430f7863b5097e644838409fc196fd605d7
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2023-07-25 19:40:41 +0000

    In EntityCollection, accept entities, typeIndex and bundleIndex params, add exportData() and importData(), add _addToBundleIndex() and _removeFromBundleIndex() and use them in add() and remove()

--- js/EntityCollection.js
+++ js/EntityCollection.js
@@ -10,17 +10,24 @@ class EntityCollection {
 
   constructor({
     id,
-    size
+    size,
+    entities = [],
+    typeIndex = {},
+    bundleIndex = {}
   }) {
 
     this._id = id
     this._size = size
 
-    this._entities = []
-    this._typeIndex = {}
+    this._entities = entities
+    this._typeIndex = typeIndex
+    this._bundleIndex = bundleIndex
 //    this._idIndex = {}
 
   }
+
+  exportData() { return this }
+  importData(data) { }
 
   getId() { return this._id }
   setId(id) { this._id = id }
@@ -36,14 +43,18 @@ class EntityCollection {
 
   add(type, entity) {
     let i = this._entities.length
+    console.log(`adding ${entity.type} ${type} ${entity.id} to ${i}...`)
     this._entities.push(entity)
     this._addToTypeIndex(type, entity, i)
+    this._addToBundleIndex(type, entity, i)
 //    this._addToIdIndex(type, entity)
   }
   remove(type, entity) {
     let i = this._typeIndex[type][entity.id]
+    console.log(`removing ${entity.type} ${type} ${entity.id} from ${i}...`)
     this._entities.splice(i, 1)
     this._removeFromTypeIndex(type, entity)
+    this._removeFromBundleIndex(type, entity)
 //    this._removeFromIdIndex(type, entity)
   }
 
@@ -52,6 +63,16 @@ class EntityCollection {
     this._typeIndex[type][entity.id] = i
   }
   _removeFromTypeIndex(type, entity) { delete this._typeIndex[type][entity.id] }
+
+  _addToBundleIndex(type, entity) {
+    if (!this._bundleIndex[type]) { this._bundleIndex[type] = {} }
+    if (!this._bundleIndex[type][entity.type]) { this._bundleIndex[type][entity.type] = [] }
+    this._bundleIndex[type][entity.type].push(entity.id)
+  }
+  _removeFromBundleIndex(type, entity) {
+    let i = this._bundleIndex[type][entity.type].indexOf(entity.id)
+    this._bundleIndex[type][entity.type].splice(i, 1)
+  }
 
 //  _addToIdIndex(type, entity) {
 //    if (!this._idIndex[type]) { this._idIndex[type] = [] }

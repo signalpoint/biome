27906c77e4b1adef406dec82c6571e6d4cc7503e
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2023-07-27 13:47:33 +0000

    Improve NpcWidget to work with index, show the npc's name and belt inventory

--- js/NpcWidget.js
+++ js/NpcWidget.js
@@ -2,7 +2,8 @@ class NpcWidget extends DesignerWidget {
 
   constructor({
     id,
-    op = 'info'
+    op = 'info',
+    npcId
   }) {
 
     super({
@@ -10,23 +11,36 @@ class NpcWidget extends DesignerWidget {
       op
     })
 
+    this._npcId = npcId
+
   }
 
   // methods
 
-  getNpc() {
-//    return d.npcs[this.delta]
-    // TODO must have Npc extend Entity and implement an Entity Index to be able to easily load Npcs from
-    return null
-  }
+  getNpc() { return d.getNpc(this._npcId) }
+
   getAddWorkerBtn() { return this.getElement().querySelector('button[data-op="addWorker"]') }
   getRemoveWorkerBtns() { return this.getElement().querySelectorAll('button[data-op="removeWorker"]') }
 
   getPaneContent(op) {
 
-//    let npc = this.getNpc()
+    let html = ''
 
-    return op
+    let npc = this.getNpc()
+
+    // BELT
+    if (npc.belt && !npc.belt.isEmpty()) {
+      let items = []
+      for (let i = 0; i < npc.belt.getSize(); i++) {
+        let item = npc.belt.get(i)
+        if (item) {
+          items.push(`<li class="list-group-item">${item.type}</li>`)
+        }
+      }
+      html += `<ul class="list-group">${items.join('')}</ul>`
+    }
+
+    return html
 
   }
 
@@ -54,29 +68,24 @@ class NpcWidget extends DesignerWidget {
 
 }
 
-function getNpcWidgetId(id) { return 'npcWidget' + id }
+function getNpcWidgetId(npcId) { return 'npcWidget' + npcId }
 
-function loadNpcWidget(id) { return _designerWidgets[getNpcWidgetId(id)] }
+function loadNpcWidget(npcId) { return _designerWidgets[getNpcWidgetId(npcId)] }
 
-function createNpcWidget(id) {
-
+function createNpcWidget(npcId) {
+  let npc = d.getNpc(npcId)
   let template = document.getElementById('npcWidget')
   let newWidget = template.cloneNode(true);
-  let newId = getNpcWidgetId(id)
+  let newId = getNpcWidgetId(npcId)
   newWidget.setAttribute('id', newId)
   template.after(newWidget)
   widget = new NpcWidget({
-    id: newId
+    id: newId,
+    npcId: npcId
   })
   widget.save()
   widget.init()
-
-  // TODO gotta have entity index working to easily load npcs
-//  let npc = d.npcs[delta]
-
-  widget.setTitle('npc.name')
+  widget.setTitle(npc.name)
   widget.refresh()
-
   return widget
-
 }

377be2a8ebd5923ac4f571a7d78783fd39406b30
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2023-08-13 12:37:28 +0000

    Inherit player movement btn listeners

--- js/DesignerPlayer.js
+++ js/DesignerPlayer.js
@@ -10,6 +10,20 @@ class DesignerPlayer {
 
   }
 
+  init() {
+
+    // player: movement
+    playerMoveUpBtn.addEventListener('mousedown', function() { keys.up.pressed = true })
+    playerMoveUpBtn.addEventListener('mouseup', function() { keys.up.pressed = false })
+    playerMoveDownBtn.addEventListener('mousedown', function() { keys.down.pressed = true })
+    playerMoveDownBtn.addEventListener('mouseup', function() { keys.down.pressed = false })
+    playerMoveLeftBtn.addEventListener('mousedown', function() { keys.left.pressed = true })
+    playerMoveLeftBtn.addEventListener('mouseup', function() { keys.left.pressed = false })
+    playerMoveRightBtn.addEventListener('mousedown', function() { keys.right.pressed = true })
+    playerMoveRightBtn.addEventListener('mouseup', function() { keys.right.pressed = false })
+
+  }
+
   refreshCoordinatesBadge() {
     this.playerCoordinatesBadge.innerHTML = player.x + ', ' + player.y
   }

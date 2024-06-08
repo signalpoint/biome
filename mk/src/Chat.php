c9b56824856d4c398f184c766676ce0652403d10
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2024-06-07 21:42:12 -0400

    white space

--- mk/src/Chat.php
+++ mk/src/Chat.php
@@ -1,9 +1,12 @@
 <?php
+
 namespace MyApp;
+
 use Ratchet\MessageComponentInterface;
 use Ratchet\ConnectionInterface;
 
 class Chat implements MessageComponentInterface {
+
     protected $clients;
 
     public function __construct() {

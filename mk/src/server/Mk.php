3be32e979f2f0240be96c5a8437a82a0483ea471
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2024-06-10 22:39:38 -0400

    Add static function to Mk - api(), q(), arg(), t()

--- mk/src/server/Mk.php
+++ mk/src/server/Mk.php
@@ -19,6 +19,34 @@ class Mk {
 
   }
 
+  public static function api() {
+    $args = self::arg();
+    $service = mb_convert_case($args[1], MB_CASE_TITLE, "UTF-8");
+    $resource = mb_convert_case($args[2], MB_CASE_TITLE, "UTF-8");
+    $apiClassName = "MaltKit\\Api{$service}{$resource}";
+    if (!class_exists($apiClassName)) { $apiClassName = "MaltKit\\Api"; }
+    return new $apiClassName;
+  }
+
+  /**
+    * Gets the "q" parameter from the query string.
+    * @return type {String}
+    */
+  public static function q() {
+    return filter_input(INPUT_GET, 'q', FILTER_SANITIZE_URL);
+  }
+
+  public static function arg($position = NULL) {
+    $q = self::q();
+    $arg = !!$q ? explode('/', $q) : NULL;
+    if ($arg && $position !== NULL) {
+      return isset($arg[$position]) ? $arg[$position] : NULL;
+    }
+    return $arg;
+  }
+
+  public static function t($text) { return $text; } // TODO internationalization
+
   public function getServer() { return $this->server; }
   public function setServer($server) { $this->server = $server; }
 
